904b33dbbbca22552503b9cfe9a483cb90bcd303
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2022-11-06 10:10:48 +0900

    Ch5[098] : 이해 돕기 위한 설명 추가

--- JavaScript/Chapter 5/098 Extracting a copy-on-write pattern.js
+++ JavaScript/Chapter 5/098 Extracting a copy-on-write pattern.js
@@ -6,6 +6,11 @@ function add_item(cart, item) {
 }
 
 /// General
+/**
+ * 장바구니와 제품에만 쓸 수 있는 함수가 아닌 어떤 배열이나 항목에도 쓸 수 있는 이름으로 바꿨다.
+ * 그래서 재사용할 수 있는 utility 함수가 되었다. 앞으로 장바구니에 제품을 추가하는 것 뿐만
+ * 아니라 배열에 항목을 추가할 일이 또 있을 때 사용할 수 있다.
+ */
 function add_element_last(array, elem) {
   var new_array = array.slice();
   new_array.push(elem);
